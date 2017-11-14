b77c77e440d5dabc50d7eb854edf1801a720e271
captainbrad <bradleylebetkin@gmail.com>
2017-11-14 14:38:47 +0000

    overhaulllllllllllllllll

--- js/app.js
+++ js/app.js
@@ -1,50 +1,36 @@
-// Smooth Scroll on clicking nav items
-$(()=>{
-
-  $('nav a').click(function () {
-    var $href = $(this).attr('href');
-    $('body').stop().animate({
-      scrollTop: $($href).offset().top
-    }, 1000);
-    return false;
-  });
-
-  // back to top
-  $('#toTop a').click(function () {
-    $('body').animate({
-      scrollTop: 0
-    }, 1000);
-    return false;
-  });
-
-  // Parallaxing + add class active on scroll
-  $(document).scroll(function () {
-
-    // parallaxing
-    var $movebg = $(window).scrollTop() * -0.3;
-    $('.portion').css('background-positionY', ($movebg) + 'px');
-
-    // add class active to nav a on scroll
-    var scrollPos = $(document).scrollTop() + 100;
-    $('nav a').each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr('href'));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-        $('nav a').removeClass('active');
-        currLink.addClass('active');
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+      &&
+      location.hostname === this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(':focus')) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        });
       }
-    });
-
-    // changing padding of nav a on scroll
-    if (scrollPos > 250) {
-      $('nav a').addClass('small');
-      $('nav img').addClass('move');
-      $('nav span').removeClass('movetext');
-    } else {
-      $('nav a').removeClass('small');
-      $('nav img').removeClass('move');
-      $('nav span').addClass('movetext');
     }
-
   });
-});
